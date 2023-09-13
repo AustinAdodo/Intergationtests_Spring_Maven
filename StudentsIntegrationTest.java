c08560f446ef4ce647690be0112a1128c1bbe073
Odia Adodo <subzbelow@gmail.com>
2023-09-13 18:29:26 +0100

    Initial

---
+++ StudentsIntegrationTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StudentsIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetStudentsEndpoint() throws Exception {
+        // Make an API call to your endpoint
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/students"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        // Deserialize the JSON response into the Students class
+        ObjectMapper objectMapper = new ObjectMapper();
+        Students students = objectMapper.readValue(result.getResponse().getContentAsString(), Students.class);
+
+        // Perform assertions on the students object
+        // For example, check if it contains the expected data
+        // assertEquals(expectedStudents, students);
+    }
+}
